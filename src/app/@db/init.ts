ef7587ef6ff2512b213a453552050f0441f63fbf
Juraji <github@juraji.nl>
2025-07-19 23:23:18 +0200

    Refactor database initialization to separate migrations into a dedicated file and improve code organization

--- src/app/@db/init.ts
+++ src/app/@db/init.ts
@@ -1,17 +1,6 @@
+import {CURRENT_VERSION, MIGRATIONS} from './migrations';
+
 export const DATABASE_NAME = 'ChatQuestStore'
-
-// Current highest version
-const CURRENT_VERSION = 1
-const DEFAULT_STORE_OPTS: IDBObjectStoreParameters = {autoIncrement: true, keyPath: 'id'}
-
-// {[target version]: [migrator]}
-const MIGRATIONS: Record<number, (db: IDBDatabase) => void> = {
-  1: db => {
-    db.createObjectStore('characters', DEFAULT_STORE_OPTS)
-    db.createObjectStore('tags', DEFAULT_STORE_OPTS)
-    db.createObjectStore('system-prompts', DEFAULT_STORE_OPTS)
-  },
-}
 
 /**
  * Initializes the IndexedDB database and applies necessary migrations.
@@ -30,19 +19,19 @@ const MIGRATIONS: Record<number, (db: IDBDatabase) => void> = {
  * @returns A promise that resolves when the database is successfully initialized or rejects on error.
  */
 export function initializeDatabase(): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const request = indexedDB.open(DATABASE_NAME, CURRENT_VERSION);
-    request.onerror = () => reject(request.error);
-    request.onsuccess = () => resolve()
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open(DATABASE_NAME, CURRENT_VERSION);
+        request.onerror = () => reject(request.error);
+        request.onsuccess = () => resolve()
 
-    request.onupgradeneeded = e => {
-      if (!e.newVersion) return
-      const db: IDBDatabase = request.result;
-      for (let i = e.oldVersion + 1; i <= e.newVersion; i++) {
-        const migration = MIGRATIONS[i]
-        if (!!migration) migration(db)
-      }
-    }
-  });
+        request.onupgradeneeded = e => {
+            if (!e.newVersion) return
+            const db: IDBDatabase = request.result;
+            for (let i = e.oldVersion + 1; i <= e.newVersion; i++) {
+                const migration = MIGRATIONS[i]
+                if (!!migration) migration(db)
+            }
+        }
+    });
 }
 
---
+++ src/app/@db/migrations.ts
@@ -0,0 +1,12 @@
+// Current highest version
+export const CURRENT_VERSION = 1
+const DEFAULT_STORE_OPTS: IDBObjectStoreParameters = {autoIncrement: true, keyPath: 'id'}
+
+// {[target version]: [migrator]}
+export const MIGRATIONS: Record<number, (db: IDBDatabase) => void> = {
+    1: db => {
+        db.createObjectStore('characters', DEFAULT_STORE_OPTS)
+        db.createObjectStore('tags', DEFAULT_STORE_OPTS)
+        db.createObjectStore('system-prompts', DEFAULT_STORE_OPTS)
+    },
+}
