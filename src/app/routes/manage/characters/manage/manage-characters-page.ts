bd0af15950bc089edddff39b83158cbc44f41c1c
Juraji <github@juraji.nl>
2025-07-19 16:29:50 +0200

    Add tag filter functionality to manage characters page

--- src/app/routes/manage/characters/manage/manage-characters-page.ts
+++ src/app/routes/manage/characters/manage/manage-characters-page.ts
@@ -1,8 +1,10 @@
-import {Component, inject, Signal} from '@angular/core';
+import {Component, computed, inject, signal, Signal, WritableSignal} from '@angular/core';
 import {routeDataSignal} from '@util/ng';
 import {ActivatedRoute, RouterLink} from '@angular/router';
 import {CharacterCard} from '@components/character-card/character-card';
 import {Character} from '@db/characters';
+import {Tag, Tags} from '@db/tags';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-manage-characters-page',
@@ -15,6 +17,27 @@ import {Character} from '@db/characters';
 })
 export class ManageCharactersPage {
   private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly tags = inject(Tags);
 
   readonly characters: Signal<Character[]> = routeDataSignal(this.activatedRoute, 'characters');
+
+  readonly availableTags: Signal<Tag[]> = toSignal(this.tags.getAll(), {initialValue: []})
+  readonly selectedTag: WritableSignal<Tag | null> = signal(null)
+
+  readonly filteredCharacters = computed(() => {
+    const characters = this.characters()
+    const selectedTag = this.selectedTag()
+    if (!!selectedTag) {
+      return characters.filter((char) => char.tagIds.includes(selectedTag.id))
+    } else {
+      return characters
+    }
+  })
+
+  onToggleSelectedTag(tag: Tag | null) {
+    this.selectedTag.update(current => {
+      if (tag == null) return null
+      return current?.id == tag.id ? null : tag;
+    })
+  }
 }
