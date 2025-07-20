76b0a76259d5af07ba896b9d27eb97875872dfc3
Juraji <github@juraji.nl>
2025-07-20 16:03:16 +0200

    Fix issue where character edit would not display likely actions, unlikely actions or dialog examples from db entry (on load)

--- src/app/routes/manage/characters/edit/components/character-edit-extended-details/character-edit-extended-details.ts
+++ src/app/routes/manage/characters/edit/components/character-edit-extended-details/character-edit-extended-details.ts
@@ -1,4 +1,4 @@
-import {Component, computed, input, InputSignal, Signal} from '@angular/core';
+import {Component, computed, effect, input, InputSignal, Signal} from '@angular/core';
 import {Character} from '@db/characters';
 import {FormArray, ReactiveFormsModule, Validators} from '@angular/forms';
 import {formControl, TypedFormArray, TypedFormGroup} from '@util/ng';
@@ -12,6 +12,7 @@ import {formControl, TypedFormArray, TypedFormGroup} from '@util/ng';
 })
 export class CharacterEditExtendedDetails {
   readonly parentForm: InputSignal<TypedFormGroup<Character>> = input.required()
+  readonly character: InputSignal<Character> = input.required()
 
   readonly dialogExample: string = `{user}: Hi, I am User. What's your name?
 {char}: *looks at {user} startled* "Ow, hello. I didn't notice you there. Nice to meet you, my name is {char}."`
@@ -23,27 +24,65 @@ export class CharacterEditExtendedDetails {
   readonly dialogueExamplesFA: Signal<TypedFormArray<string>> =
     computed(() => this.parentForm().get('dialogueExamples') as FormArray)
 
+  constructor() {
+    effect(() => {
+      const {
+        likelyActions,
+        unlikelyActions,
+        dialogueExamples,
+      } = this.character()
+
+      const la = this.likelyActionsFA()
+      la.clear()
+      likelyActions.forEach(action => this.addControl(la, action));
+
+      const ua = this.unlikelyActionsFA()
+      ua.clear()
+      unlikelyActions.forEach(action => this.addControl(ua, action));
+
+      const de = this.dialogueExamplesFA()
+      de.clear()
+      dialogueExamples.forEach(action => this.addControl(de, action));
+    });
+  }
+
   onAddLikelyAction() {
-    this.likelyActionsFA().push(formControl('', [Validators.required]))
+    const fa = this.likelyActionsFA()
+    this.addControl(fa)
+    fa.markAsDirty()
   }
 
   onRemoveLikelyAction(idx: number) {
-    this.likelyActionsFA().removeAt(idx)
+    const fa = this.likelyActionsFA()
+    fa.removeAt(idx)
+    fa.markAsDirty()
   }
 
   onAddUnlikelyAction() {
-    this.unlikelyActionsFA().push(formControl('', [Validators.required]))
+    const fa = this.unlikelyActionsFA()
+    this.addControl(fa)
+    fa.markAsDirty()
   }
 
   onRemoveUnlikelyAction(idx: number) {
-    this.unlikelyActionsFA().removeAt(idx)
+    const fa = this.unlikelyActionsFA()
+    fa.removeAt(idx)
+    fa.markAsDirty()
   }
 
   onAddDialogExample() {
-    this.dialogueExamplesFA().push(formControl('', [Validators.required]))
+    const fa = this.dialogueExamplesFA()
+    this.addControl(fa)
+    fa.markAsDirty()
   }
 
   onRemoveDialogExample(idx: number) {
-    this.dialogueExamplesFA().removeAt(idx)
+    const fa = this.dialogueExamplesFA()
+    fa.removeAt(idx)
+    fa.markAsDirty()
+  }
+
+  private addControl(arr: TypedFormArray<string>, value: string = '') {
+    arr.push(formControl(value, [Validators.required]))
   }
 }
