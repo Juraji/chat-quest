0ac98dc56775d3893acecf8e8b71331828480f54
Juraji <github@juraji.nl>
2025-08-20 15:13:04 +0200

    Enable foreign keys in SQLite by adding _foreign_keys=true to the connection string and remove redundant PRAGMA statement

--- backend/core/database/database.go
+++ backend/core/database/database.go
@@ -1,47 +1,41 @@
 package database
 
 import (
-	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
-	"go.uber.org/zap"
-	"juraji.nl/chat-quest/core/log"
+  "database/sql"
+  _ "github.com/mattn/go-sqlite3"
+  "go.uber.org/zap"
+  "juraji.nl/chat-quest/core/log"
 )
 
 var dbInstance *sql.DB
 
 func GetDB() *sql.DB {
-	if dbInstance == nil {
-		panic("database not initialized")
-	}
+  if dbInstance == nil {
+    panic("database not initialized")
+  }
 
-	return dbInstance
+  return dbInstance
 }
 
 // InitDB sets up the database, populates dbInstance and runs any migrations.
 func InitDB() func() {
-	// DB Setup
-	dbLogger := log.Get()
-	db, err := sql.Open("sqlite3", "./chat-quest.db")
-	if err != nil {
-		dbLogger.Fatal("Failed to connect to database", zap.Error(err))
-	}
-	dbInstance = db
-	closeDB := func() {
-		err := db.Close()
-		if err != nil {
-			dbLogger.Fatal("Failed to close database", zap.Error(err))
-		}
-	}
+  // DB Setup
+  dbLogger := log.Get()
+  db, err := sql.Open("sqlite3", "./chat-quest.db?_foreign_keys=true")
+  if err != nil {
+    dbLogger.Fatal("Failed to connect to database", zap.Error(err))
+  }
+  dbInstance = db
+  closeDB := func() {
+    err := db.Close()
+    if err != nil {
+      dbLogger.Fatal("Failed to close database", zap.Error(err))
+    }
+  }
 
-	// SQLite specific
-	_, err = db.Exec("PRAGMA foreign_keys = ON;")
-	if err != nil {
-		dbLogger.Fatal("Failed to set foreign keys pragma for SQLite", zap.Error(err))
-	}
+  // Run migrations (will panic if fails)
+  runLatestMigrations(db)
 
-	// Run migrations (will panic if fails)
-	runLatestMigrations(db)
-
-	// Return the db closer
-	return closeDB
+  // Return the db closer
+  return closeDB
 }
