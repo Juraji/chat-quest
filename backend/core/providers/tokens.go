27465bf0a3aa28d87d3e04b1f254301d79f3bc5e
Juraji <github@juraji.nl>
2025-08-21 11:13:13 +0200

    Use CL100kBase vocabulary (instead of O200kBase) for token counts

--- backend/core/providers/tokens.go
+++ backend/core/providers/tokens.go
@@ -6,7 +6,7 @@ import (
 
 // TokenCount calculates the number of tokens in the given text using the GPT-4o tokenizer.
 func TokenCount(text string) (int, error) {
-	enc, err := tokenizer.Get(tokenizer.O200kBase)
+	enc, err := tokenizer.Get(tokenizer.Cl100kBase)
 	if err != nil {
 		return 0, err
 	}
