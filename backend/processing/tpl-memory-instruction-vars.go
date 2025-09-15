ebe752204bd414eb7ccde689e1eb7e00ca63ee39
Juraji <github@juraji.nl>
2025-09-15 23:47:26 +0200

    Added Persona as sparse character to the memory instruction variables, so the LLM knows what the user is called (if a persona is set)

--- backend/processing/tpl-memory-instruction-vars.go
+++ backend/processing/tpl-memory-instruction-vars.go
@@ -4,19 +4,26 @@ import (
 	"sync"
 	"time"
 
+	c "juraji.nl/chat-quest/model/characters"
 	cs "juraji.nl/chat-quest/model/chat-sessions"
+	w "juraji.nl/chat-quest/model/worlds"
 )
 
 type MemoryInstructionVars interface {
 	Participants() ([]TemplateCharacter, error)
+	Persona() (SparseTemplateCharacter, error)
 }
 
 type memoryInstructionVarsImpl struct {
 	participants func() ([]TemplateCharacter, error)
+	persona      func() (SparseTemplateCharacter, error)
 }
 
 func (m memoryInstructionVarsImpl) Participants() ([]TemplateCharacter, error) {
 	return m.participants()
+}
+func (m memoryInstructionVarsImpl) Persona() (SparseTemplateCharacter, error) {
+	return m.persona()
 }
 
 func NewMemoryInstructionVars(session *cs.ChatSession, before time.Time) MemoryInstructionVars {
@@ -31,6 +38,20 @@ func NewMemoryInstructionVars(session *cs.ChatSession, before time.Time) MemoryI
 				templateVars[i] = NewTemplateCharacter(&participant, nil, nil, nil)
 			}
 			return templateVars, nil
+		}),
+		persona: sync.OnceValues(func() (SparseTemplateCharacter, error) {
+			world, err := w.WorldById(session.WorldID)
+			if err != nil {
+				return nil, err
+			}
+			if world.PersonaID == nil {
+				return nil, nil
+			}
+			character, err := c.CharacterById(*world.PersonaID)
+			if err != nil {
+				return nil, err
+			}
+			return NewSparseTemplateCharacter(character), nil
 		}),
 	}
 }
