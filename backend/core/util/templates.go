7db2a1d96ad9706c9cb94d26512a780ca149f197
Juraji <github@juraji.nl>
2025-09-21 00:41:44 +0200

    Add template functions for slicing and shuffling strings in templates

--- backend/core/util/templates.go
+++ backend/core/util/templates.go
@@ -2,23 +2,26 @@ package util
 
 import (
 	"bytes"
+	"math/rand"
 	"strings"
 	gt "text/template"
 
 	"github.com/pkg/errors"
 )
 
-func ParseAndApplyTextTemplate(
-	template string,
-	variables any,
-) (string, error) {
+var templateFuncMap = gt.FuncMap{
+	"sliceTakeStr":    tplSliceTakeStr,
+	"sliceTakeStrRnd": tplSliceTakeStrRnd,
+}
+
+func ParseAndApplyTextTemplate(template string, variables any) (string, error) {
 	if len(template) == 0 || !strings.Contains(template, "{{") {
 		// Shortcut: Template has no variables
 		return template, nil
 	}
 
 	tplName := "Template: " + template[:20] + "..."
-	tpl, err := gt.New(tplName).Parse(template)
+	tpl, err := gt.New(tplName).Funcs(templateFuncMap).Parse(template)
 	if err != nil {
 		return "", errors.Wrap(err, "Failed to parse template")
 	}
@@ -30,4 +33,28 @@ func ParseAndApplyTextTemplate(
 	}
 
 	return buffer.String(), nil
+}
+
+func tplSliceTakeStr(slice []string, limit int) []string {
+	if len(slice) <= limit {
+		return slice
+	} else {
+		return slice[:limit-1]
+	}
+}
+
+func tplSliceTakeStrRnd(slice []string, limit int) []string {
+	if len(slice) <= limit {
+		return slice
+	}
+
+	shuffled := make([]string, len(slice))
+	copy(shuffled, slice)
+
+	for i := range shuffled {
+		j := rand.Intn(i + 1)
+		shuffled[i], shuffled[j] = shuffled[j], shuffled[i]
+	}
+
+	return shuffled[:limit]
 }
